c1d374590bf4432e38d82a29d3aaa15376c39ea6
GuilhermePagglo <guilherme.soares@pagglo.com>
2024-05-23 11:00:06 -0300

    implementando vueX

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: {
+    //  estado inicial
+  },
+  mutations: {
+    //  mutações
+  },
+  actions: {
+    //  ações
+  },
+  getters: {
+    //  getters
+  }
+})
+
+export default store
